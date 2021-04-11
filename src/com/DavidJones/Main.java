df42c666103969bd2d10de857fb72350aebdf714
David Jones <coreldavid@hotmail.com>
2021-04-11 01:57:27 -0400

    Day 2

---
+++ src/com/DavidJones/Main.java
@@ -0,0 +1,13 @@
+package com.DavidJones;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello");
+        HelloWorldMine HW  = new HelloWorldMine();
+
+        System.out.println("\n");
+        HW.sayIt();
+    }
+}
